7f5f7cade7e93b141cca76fb6f8daf5d2bea86b4
Emma <emckenziee@ymail.com>
2023-12-01 18:59:25 +0000

    day 1 part 1

--- day1/day_1_code.py
+++ day1/day_1_code.py
@@ -2,4 +2,23 @@ from utilities.parsing import *
 
 input_file = '..//inputs//day1.txt'
 
-print(read_in_file_to_int_array(input_file))
+
+def main_fun():
+    count = 0
+    str_list = read_in_file_to_str_list(input_file)
+    for line in str_list:
+        count += int(get_calibration(line))
+    print(count)
+    return count
+
+
+def get_calibration(line):
+    string_of_digits = ""
+
+    for character in line:
+        if character.isdigit():
+            string_of_digits += character
+    return string_of_digits[0] + string_of_digits[len(string_of_digits) - 1]
+
+
+main_fun()
