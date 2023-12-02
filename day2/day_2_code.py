d0b3a418ddd58ebd5c2aaddf9c5fa85e658b8c4a
emmasTC <emmas.tech.corner@gmail.com>
2023-12-02 22:32:46 +0000

    wip

--- day2/day_2_code.py
+++ day2/day_2_code.py
@@ -7,27 +7,40 @@ def main_fun():
     bag_contents = {"red": 12, "green": 13, "blue": 14}
     games = parse_input()
 
+def colour_list(game):
+    print("in colour list")
+    edited_string = game.replace(";", ",")
+    print(edited_string)
+    print(edited_string.split(","))
+    print("edited string " + edited_string)
+
 def max_cubes_shown(colour, game):
-    current_max = 0
+
+    colour_list(game)
+
     instances_of_colour = game.count(colour)
 
-    index = game.find(colour)
+    if instances_of_colour == 0:
+        return 0
 
-    print(game[index-2])
-    return game[index-2]
+    # from space reverse to space
+    # for x in range(instances_of_colour):
+    #     index = game.find(colour)
+    #     print(int(game[index - 2]))
 
 def parse_input():
-    game_list =[]
+    game_list = []
 
     str_list = read_in_file_to_str_list(input_file)
     print(str_list)
 
     for game in str_list:
+        game_start = game.find(":") + 1
         game_dictionary = {
             "id": game[5],
-            "blue": max_cubes_shown("blue", game),
-            "red": max_cubes_shown("red", game),
-            "green": max_cubes_shown("green", game)
+            "blue": max_cubes_shown("blue", game[game_start:]),
+            "red": max_cubes_shown("red", game[game_start:]),
+            "green": max_cubes_shown("green", game[game_start])
         }
 
         game_list.append(game_dictionary)
