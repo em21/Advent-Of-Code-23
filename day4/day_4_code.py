8a93f00fc6ef4f9a508ba3af89456af773b45603
Emma <emckenziee@ymail.com>
2023-12-05 21:38:20 +0000

    day 4 - optimise for part 2

--- day4/day_4_code.py
+++ day4/day_4_code.py
@@ -7,20 +7,26 @@ def main_fun():
     lines = file1.readlines()
 
     for line in lines:
-        card_score = 0
         game_card = line[line.index(":") + 1:]
         game_list = game_card.strip().split("|")
         winning_numbers = game_list[0].strip().replace(" ", ",").split(",")
         lottery_numbers = game_list[1].strip().replace("  ", " ").replace(" ", ",").split(",")
 
-        for winning_number in winning_numbers:
-            if winning_number in lottery_numbers:
-                if card_score == 0:
-                    card_score = 1
-                else:
-                    card_score *= 2
+        card_score = get_card_score(winning_numbers, lottery_numbers)
+
         total_point += card_score
     return total_point
+
+
+def get_card_score(winning_numbers, lottery_numbers):
+    card_score = 0
+    for winning_number in winning_numbers:
+        if winning_number in lottery_numbers:
+            if card_score == 0:
+                card_score = 1
+            else:
+                card_score *= 2
+    return card_score
 
 
 print(main_fun())
